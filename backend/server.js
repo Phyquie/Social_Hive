07ba37f54ab775caf2b65e94c7d67401d10ecc10
AYUSH <ayushking6395@gmail.com>
2024-08-29 12:31:38 +0530

    final-commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
@@ -16,6 +17,7 @@ api_secret: process.env.CLOUDINARY_API_SECRET,
 
 const app = express();
 const Port = process.env.Port;
+const __dirname=path.resolve();
 app.use(express.json({limit:"500kb"}));
 app.use(express.json());//to parse req.body
 app.use(express.urlencoded({extended:true}));//to parse form data'
@@ -24,6 +26,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notifications",notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(Port, () => {
   console.log(`Server started at http://localhost:${Port}`);
